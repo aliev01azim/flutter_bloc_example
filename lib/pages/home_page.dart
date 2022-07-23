98161e3bf5557c67e89bc2f2ffa79aad4e9a7196
azim <aliev01azim@gmail.com>
2022-07-24 04:02:28 +0600

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter_bloc_example/bloc/user_bloc.dart';
+import 'package:flutter_bloc_example/bloc/user_event.dart';
+import 'package:flutter_bloc_example/services/user_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../widgets/action_buttons.dart';
+import '../widgets/user_list.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  // final userRepository = UserRepository();
+
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider(
+      create: (context) => UserRepository(),
+      child: BlocProvider(
+        create: (context) =>
+            UserBloc(userRepository: context.read<UserRepository>())
+              ..add(UserLoadEvent()),
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('User List'),
+            centerTitle: true,
+          ),
+          backgroundColor: Colors.white,
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: const [
+              ActionButtons(),
+              Expanded(child: UserList()),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
